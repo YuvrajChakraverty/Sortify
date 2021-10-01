89587ebccac008628639a54b5bef843ee2208c12
YuvrajChakraverty <yuvrajchakraverty007@gmail.com>
2021-10-01 14:38:25 +0530

    First commit: app.js added

---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const app = express();
+const port = 80;
+const path = require("path");
+const fs = require("fs");
+const bodyparser = require("body-parser");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/sortify', { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("--- connection est. w/ sortify db ---");
+});
+
+
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get("/", (req, res) => {
+    res.status(200).render("home");
+})
+
+app.get("/artists", (req, res) => {
+    res.status(200).render("artists");
+})
+
+app.get("/playlists", (req, res) => {
+    res.status(200).render("playlists");
+})
+
+app.get("/sign-in", (req, res) => {
+    res.status(200).render("sign-in");
+})
+
+app.post("/sign-in", (req, res) => {
+    res.status(200).render("unavailable");
+})
+
+app.get("/sign-up", (req, res) => {
+    res.status(200).render("sign-up");
+})
+
+app.post("/sign-up", (req, res) => {
+    var userData = new User(req.body);
+    userData.save().then(()=>{
+    res.status(200).render("u_acc_created");
+    }).catch(()=>{
+    res.status(400).send("Error saving!");
+    })
+})
+
+app.get("/unavailable", (req, res) => {
+    res.status(200).render("unavailable");
+})
+
+app.post("/unavailable", (req, res) => {
+    res.status(200).render("unavailable");
+})
+
+app.get("/T&C", (req, res) => {
+    res.status(200).render("u_T&C");
+})
+
+app.get("/about", (req, res) => {
+    res.status(200).render("z_about");
+})
+
+app.get("/contactus", (req, res) => {
+    res.status(200).render("z_contactus");
+})
+
+app.get("/community", (req, res) => {
+    res.status(200).render("z_community");
+})
+
+app.get("/feedback", (req, res) => {
+    res.status(200).render("z_feedback");
+})
+
+app.listen(port, () => {
+    console.log(`--- 'Sortify' is live on port: ${port} ---`);
+});
